ef961b3f133367ca39298d11ea1ed3bdd752275b
robert-clayton <41345902+robert-clayton@users.noreply.github.com>
2021-10-14 08:40:36 -0700

    Add empty tests folder

--- scrolller/__main__.py
+++ scrolller/__main__.py
@@ -1,4 +1,5 @@
 from scrolller.main import main
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- scrolller/main.py
+++ scrolller/main.py
@@ -1,36 +1,53 @@
-import os
-from pathlib import Path
-import sys
 import ctypes
+import os
 import random
-from PySide6.QtWidgets import QApplication
-from PySide6.QtQml import QQmlApplicationEngine
-from PySide6.QtCore import QSortFilterProxyModel, Qt, QSettings, QUrl, QObject, Signal, Slot, QStandardPaths, \
-        QtInfoMsg, QtWarningMsg, QtCriticalMsg, QtFatalMsg,  QAbstractListModel, QModelIndex, \
-        Property, Signal
-from PySide6.QtGui import QIcon
+import sys
+from pathlib import Path
+
 from PIL import Image
+from PySide6.QtCore import (
+    Property,
+    QAbstractListModel,
+    QModelIndex,
+    QObject,
+    QSettings,
+    QSortFilterProxyModel,
+    QStandardPaths,
+    Qt,
+    QtCriticalMsg,
+    QtFatalMsg,
+    QtInfoMsg,
+    QtWarningMsg,
+    QUrl,
+    Signal,
+    Slot,
+)
+from PySide6.QtGui import QIcon
+from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtWidgets import QApplication
 
 
 try:
-    with open(Path(__file__).resolve().parent.parent / 'VERSION') as f:
+    with open(Path(__file__).resolve().parent.parent / "VERSION") as f:
         version = f.read().strip()
 except FileNotFoundError:
-    version = '0.0.0'
+    version = "0.0.0"
 
 
 def qt_message_handler(mode, context, message):
     if mode == QtInfoMsg:
-        mode = 'Info'
+        mode = "Info"
     elif mode == QtWarningMsg:
-        mode = 'Warning'
+        mode = "Warning"
     elif mode == QtCriticalMsg:
-        mode = 'critical'
+        mode = "critical"
     elif mode == QtFatalMsg:
-        mode = 'fatal'
+        mode = "fatal"
     else:
-        mode = 'Debug'
-    print("%s: %s (%s:%d, %s)" % (mode, message, context.file, context.line, context.file))
+        mode = "Debug"
+    print(
+        "%s: %s (%s:%d, %s)" % (mode, message, context.file, context.line, context.file)
+    )
 
 
 class ImageProxy(QSortFilterProxyModel):
@@ -40,12 +57,17 @@ class ImageProxy(QSortFilterProxyModel):
         self.proxyID = proxyID
 
     def filterAcceptsRow(self, source_row, source_parent):
-        return self.sourceModel().index(source_row, 0, source_parent).data(ImageModel.proxyID) == self.proxyID
-    
+        return (
+            self.sourceModel()
+            .index(source_row, 0, source_parent)
+            .data(ImageModel.proxyID)
+            == self.proxyID
+        )
+
     @Slot()
     def generateImages(self):
         self.sourceModel().generateImages(proxyID=self.proxyID)
-    
+
     @Slot(result=int)
     def getProxyID(self):
         return self.proxyID
@@ -69,7 +91,6 @@ class ImageModel(QAbstractListModel):
         if folder is None:
             folder = self.getFolder()
         self.setFolder(folder)
-        
 
     def data(self, index, role=Qt.DisplayRole):
         row = index.row()
@@ -86,23 +107,27 @@ class ImageModel(QAbstractListModel):
 
     def roleNames(self):
         return {
-            ImageModel.url: b'url',
-            ImageModel.proxyID: b'proxyID',
-            ImageModel.ratio: b'ratio'
+            ImageModel.url: b"url",
+            ImageModel.proxyID: b"proxyID",
+            ImageModel.ratio: b"ratio",
         }
 
-    @Slot(int, result='QVariant')
+    @Slot(int, result="QVariant")
     def get(self, row):
         if 0 <= row < self.rowCount():
             return self.imageData[row]
 
     def setFolder(self, folder: QUrl):
         folder = folder.toLocalFile()
-        QSettings().setValue('folder', folder)
+        QSettings().setValue("folder", folder)
         self.beginRemoveRows(QModelIndex(), 0, self.rowCount() - 1)
         self.imageData = []
         self.endRemoveRows()
-        self.imageList = [os.path.join(folder, file) for file in os.listdir(folder) if file.endswith(('.jpg', '.png'))]
+        self.imageList = [
+            os.path.join(folder, file)
+            for file in os.listdir(folder)
+            if file.endswith((".jpg", ".png"))
+        ]
         random.shuffle(self.imageList)
         self.toGenerateList = self.imageList
         for proxy in self.proxies.values():
@@ -110,28 +135,40 @@ class ImageModel(QAbstractListModel):
 
     @Slot(result=QUrl)
     def getFolder(self):
-        return QUrl.fromLocalFile(QSettings().value('folder', QStandardPaths.standardLocations(QStandardPaths.PicturesLocation)[0]))
+        return QUrl.fromLocalFile(
+            QSettings().value(
+                "folder",
+                QStandardPaths.standardLocations(QStandardPaths.PicturesLocation)[0],
+            )
+        )
 
     def generateImageData(self, count: int, proxyID: int):
         if count > len(self.toGenerateList):
             random.shuffle(self.imageList)
             self.toGenerateList = self.imageList
         data = []
-        self.toGenerateList, generatingList = self.toGenerateList[count:], self.toGenerateList[:count]
+        self.toGenerateList, generatingList = (
+            self.toGenerateList[count:],
+            self.toGenerateList[:count],
+        )
         for path in generatingList:
             try:
                 with Image.open(path) as img:
                     ratio = img.size[0] / img.size[1]
-            except PermissionError: # file is not readable
+            except PermissionError:  # file is not readable
                 continue
-            data.append({ "url": QUrl.fromLocalFile(path), "ratio": ratio , "proxyID": proxyID })
+            data.append(
+                {"url": QUrl.fromLocalFile(path), "ratio": ratio, "proxyID": proxyID}
+            )
         return data
 
     @Slot(result=bool)
     @Slot(int, result=bool)
     @Slot(int, int, result=bool)
     def generateImages(self, count: int = 5, proxyID: int = 0):
-        self.beginInsertRows(QModelIndex(), self.rowCount(), self.rowCount() + count - 1)
+        self.beginInsertRows(
+            QModelIndex(), self.rowCount(), self.rowCount() + count - 1
+        )
         self.imageData.extend(self.generateImageData(count, proxyID))
         self.endInsertRows()
         return True
@@ -140,66 +177,76 @@ class ImageModel(QAbstractListModel):
     def requestProxy(self, proxyID: int):
         self.proxies[proxyID] = ImageProxy(self, proxyID)
         return self.proxies[proxyID]
-    
+
     @Slot(int)
     def removeProxy(self, proxyID: int):
         self.proxies.pop(proxyID, None)
 
+
 class Backend(QObject):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self._visibility = QSettings().value('visibility', 'Windowed')
-        self._tickSpeed = QSettings().value('tickSpeed', 1000)
-        self._colCount = QSettings().value('colCount', 3)
-    
+        self._visibility = QSettings().value("visibility", "Windowed")
+        self._tickSpeed = QSettings().value("tickSpeed", 1000)
+        self._colCount = QSettings().value("colCount", 3)
+
     def setVisibility(self, visibility: str):
         self._visibility = visibility
-        QSettings().setValue('visibility', visibility)
+        QSettings().setValue("visibility", visibility)
         self.visibilityChanged.emit(visibility)
-    
+
     @Slot()
     def toggleVisibility(self):
-        self.setVisibility('FullScreen' if self._visibility == 'Windowed' else 'Windowed')
-    
+        self.setVisibility(
+            "FullScreen" if self._visibility == "Windowed" else "Windowed"
+        )
+
     @Slot(int)
     def setTickSpeed(self, tickSpeed: int):
         self._tickSpeed = tickSpeed
-        QSettings().setValue('tickSpeed', tickSpeed)
+        QSettings().setValue("tickSpeed", tickSpeed)
         self.tickSpeedChanged.emit(tickSpeed)
-    
+
     @Slot(int)
     def setColCount(self, colCount: int):
         self._colCount = colCount
-        QSettings().setValue('colCount', colCount)
+        QSettings().setValue("colCount", colCount)
         self.colCountChanged.emit(colCount)
 
     visibilityChanged = Signal(str)
     tickSpeedChanged = Signal(int)
     colCountChanged = Signal(int)
-    visibility = Property(str, lambda self: self._visibility, setVisibility, notify=visibilityChanged)
-    tickSpeed = Property(int, lambda self: self._tickSpeed, setTickSpeed, notify=tickSpeedChanged)
-    colCount = Property(int, lambda self: self._colCount, setColCount, notify=colCountChanged)
+    visibility = Property(
+        str, lambda self: self._visibility, setVisibility, notify=visibilityChanged
+    )
+    tickSpeed = Property(
+        int, lambda self: self._tickSpeed, setTickSpeed, notify=tickSpeedChanged
+    )
+    colCount = Property(
+        int, lambda self: self._colCount, setColCount, notify=colCountChanged
+    )
+
 
 def main():
     app = QApplication(sys.argv)
-    app.setOrganizationName('Ziru\'s Musings')
-    app.setOrganizationDomain('github.com/robert-clayton')
-    app.setApplicationName('scrolller')
-    app.setDesktopFileName('scrolller')
+    app.setOrganizationName("Ziru's Musings")
+    app.setOrganizationDomain("github.com/robert-clayton")
+    app.setApplicationName("scrolller")
+    app.setDesktopFileName("scrolller")
     app.setWindowIcon(QIcon())
-    myappid = f'zirus-musings.scrolller.bg.{version}'
+    myappid = f"zirus-musings.scrolller.bg.{version}"
     ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(myappid)
     app.setApplicationVersion(version)
 
     backend = Backend()
     images = ImageModel()
     engine = QQmlApplicationEngine()
-    engine.rootContext().setContextProperty('Backend', backend)
-    engine.rootContext().setContextProperty('ImageModel', images)
-    gui = os.fspath(Path(__file__).resolve().parent / 'gui.qml')
+    engine.rootContext().setContextProperty("Backend", backend)
+    engine.rootContext().setContextProperty("ImageModel", images)
+    gui = os.fspath(Path(__file__).resolve().parent / "gui.qml")
     engine.load(QUrl.fromLocalFile(gui))
 
     if not engine.rootObjects():
         sys.exit(-1)
-        
+
     sys.exit(app.exec_())
