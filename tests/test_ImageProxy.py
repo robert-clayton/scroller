7d732cc469cc9fc16326f6741633566b609e4427
robert-clayton <41345902+robert-clayton@users.noreply.github.com>
2023-01-13 15:00:25 -0800

    Add imageproxy tests

---
+++ tests/test_ImageProxy.py
@@ -0,0 +1,27 @@
+import pytest
+from PySide6.QtCore import QUrl, QModelIndex
+from scroller.ImageModel import ImageModel
+
+@pytest.fixture
+def prefabs():
+    source = ImageModel()
+    source.startup(QUrl("tests/img"))
+    id = 1
+    return source, id, source.requestProxy(id)
+
+def test_filterAcceptsRow(qtbot, prefabs):
+    source, id, proxy = prefabs
+    assert proxy.filterAcceptsRow(0, QModelIndex()) == (
+        source.index(0, 0, QModelIndex()).data(ImageModel.proxyID) == id
+    )
+
+def test_generateImages(qtbot, prefabs):
+    _, _, proxy = prefabs
+    count = 5
+    proxy.generateImages(count)
+    qtbot.waitUntil(lambda: proxy.rowCount() == count, timeout=200)
+    assert proxy.rowCount() == count
+
+def test_getProxyID(qtbot, prefabs):
+    _, id, proxy = prefabs
+    assert proxy.getProxyID() == id
